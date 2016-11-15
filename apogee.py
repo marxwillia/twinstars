6df695ea3f540d510df2b82c383e463d5acca068
Marc Williamson <mew488@PD326.PHYSICS.NYU.EDU>
2016-11-15 18:17:00 -0500

    plots apogee and boss spectra, apogee with best fit

--- apogee.py
+++ apogee.py
@@ -2,16 +2,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
 
-def load_apogee_spec(path):
+def load_boss_spec(path):
     hdulist = fits.open(path)
     tbdata = hdulist[1].data
     hdulist.close()
     return tbdata
 
-def plot_spec(tbdata):
+def plot_boss_spec(tbdata,title=None):
     flux = tbdata.field('flux')
     loglam = tbdata.field('loglam')
     plt.plot(np.power(10,loglam),flux)
     plt.xlabel('Wavelength '+'$[\AA]$')
     plt.ylabel('Flux $[10^{-17}$ erg/cm$^{2}$/s/$\AA]$')
-    return 
+    plt.title(title)
+#    plt.show()
+    return plt.gcf()
+
+def load_apogee(path):
+    hdulist = fits.open(path)
+    spec = hdulist[1].data
+    spec_head = hdulist[1].header
+
+    spec_model = hdulist[3].data
+    spec_model_head = hdulist[3].header
+
+    hdulist.close()
+    return spec, spec_head, spec_model, spec_model_head
+
+def plot_apogee_spec(spec_dat, spec_head, model_dat=None, title=None, bestfit=False):
+    num = len(spec_dat)
+    start_loglam = spec_head['CRVAL1']
+    step_loglam = spec_head['CDELT1']
+    wv_log = np.arange(start_loglam, start_loglam+num*step_loglam, step_loglam)
+    plt.plot(np.power(10,wv_log), spec_dat, "k")
+
+    if bestfit:
+        plt.plot(np.power(10,wv_log), model_dat, "r")
+
+    plt.xlabel('Wavelength '+'$[\AA]$')
+    plt.ylabel('Flux $[10^{-17}$ erg/cm$^{2}$/s/$\AA]$')
+    plt.title(title)
+    return plt.gcf()
